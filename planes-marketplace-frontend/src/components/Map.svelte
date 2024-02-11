ed1098315cedb45149af486e1626e1902552069c
DzIGIBAUER <milosh2255@gmail.com>
2024-02-11 11:05:46 +0100

    Added Env vars

--- planes-marketplace-frontend/src/components/Map.svelte
+++ planes-marketplace-frontend/src/components/Map.svelte
@@ -1,14 +1,19 @@
 <svelte:head>
-	<script>
-		(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
-		  key: "AIzaSyC0Xzt7kJSt2eXmc-z4aHAl4QzM-Riyc5Q",
-		  v: "weekly",
-		  libraries: "places"
-		  // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
-		  // Add other bootstrap parameters as needed, using camel case.
-		});
-	</script>	  
+	
+	{@html `
+		<script>
+			(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=` + '`https://maps.${c}apis.com/maps/api/js?`' + `+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
+			key: '${PUBLIC_GOOGLE_MAPS_API}',
+			v: "weekly",
+			libraries: "places"
+			// Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
+			// Add other bootstrap parameters as needed, using camel case.
+			});
+		</script>	
+	`}
+	  
 </svelte:head>
+
 
 
 <div id="map" class="w-screen h-screen"></div>
@@ -16,6 +21,8 @@
 
 <script lang="ts">
 	import { onMount } from "svelte";
+
+	import { PUBLIC_GOOGLE_MAPS_API } from "$env/static/public";
 
 	import { comparePlanesStore } from '$lib/comparePlanes';
 	import { mapStyles } from "$lib";
@@ -60,7 +67,7 @@
 			document.getElementById('map') as HTMLElement,
 			{
 				zoom: 4,
-				center: { lat: -25.344, lng: 131.031 },
+				center: { lat: 0, lng: 0 },
 				styles: mapStyles,
 			}
 		);
