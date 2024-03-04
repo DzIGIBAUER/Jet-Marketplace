f7a1b1240f7f30c0cf857c24c93b6eb321d584c5
DzIGIBAUER <milosh2255@gmail.com>
2024-03-04 16:52:40 +0100

    Added more filtering options to planes selection

--- planes-marketplace-frontend/src/routes/+page.svelte
+++ planes-marketplace-frontend/src/routes/+page.svelte
@@ -14,7 +14,7 @@
 				<button type="button" class="btn variant-filled-primary" on:click={() => drawerStore.open()}>
 					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
 						<path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-					</svg>				
+					</svg>
 				</button>
 			</div>
 		</div>
@@ -59,6 +59,5 @@
 		});
 	}
 
-	
 
 </script>
