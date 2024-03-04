158905119b0b3a34888b881a83a6bfcbe33a1d56
DzIGIBAUER <milosh2255@gmail.com>
2024-03-04 17:03:21 +0100

    Added analytics

--- planes-marketplace-frontend/src/routes/+layout.svelte
+++ planes-marketplace-frontend/src/routes/+layout.svelte
@@ -2,11 +2,15 @@
 	import '../app.postcss';
 	import { initializeStores } from '@skeletonlabs/skeleton';
 	import { Drawer, getDrawerStore } from '@skeletonlabs/skeleton';
+
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics'
 	
 	import PlanesList from '../components/PlanesList.svelte';
 	import type { Plane } from '$lib';
 
 	initializeStores();
+	inject({ mode: dev ? 'development' : 'production' });
 
 	const drawerStore = getDrawerStore();
 
